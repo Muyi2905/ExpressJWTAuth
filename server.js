2e84e3dc3c468eb27920b648678421f582fa8855
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-25 21:25:22 +0000

    UPDATE

--- server.js
+++ server.js
@@ -2,25 +2,35 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
-const users = [
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
+
+const posts = [
   {
     user1: "mave",
     title: "title 1",
   },
   {
-    user1: "mave",
-    title: "title 1",
+    user1: "mavwe",
+    title: "title 2",
   },
 ];
 
-app.get("/users", (req, res) => {
+app.get("/posts", (req, res) => {
   try {
-    res.staus(200).send(users);
+    res.staus(200).send(posts);
     console.log("endpoint accessed");
   } catch (error) {
     res.staus(500).json(error);
     console.error(error);
   }
+});
+
+app.post("/login", async (req, res) => {
+  const username = await req.body.username;
+  const user = { user: username };
+  jwt.sign(user, process.env.ACCESS_SECRET_TOKEN);
 });
 
 app.listen(PORT, () => {
