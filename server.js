5f13c14bd59c97574ed2811155722c872257e01f
muyi2905 <obaremimuyiwa@gmail.com>
2024-02-26 09:36:25 +0000

    middleware added

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-<<<<<<< HEAD
-require("dotenv");
+require("dotenv").config();
 const app = express();
 const PORT = 3000;
 const jwt = require("jsonwebtoken");
@@ -8,37 +7,19 @@ const jwt = require("jsonwebtoken");
 app.use(express.json());
 
 const posts = [
-=======
-
-const app = express;
-const PORT = 3000;
-const users = [
->>>>>>> setup-branch
   {
     user1: "mave",
     title: "title 1",
   },
   {
-<<<<<<< HEAD
     user1: "mavwe",
     title: "title 2",
   },
 ];
 
-app.get("/posts", (req, res) => {
+app.get("/posts", authenticateToken, (req, res) => {
   try {
     res.status(200).send(posts);
-=======
-    user1: "mave",
-    title: "title 1",
-  },
-];
-
-app.get("/users", (req, res) => {
-  res.send(users);
-  try {
-    res.staus(200);
->>>>>>> setup-branch
     console.log("endpoint accessed");
   } catch (error) {
     res.staus(500).json(error);
@@ -46,18 +27,32 @@ app.get("/users", (req, res) => {
   }
 });
 
-<<<<<<< HEAD
 app.post("/login", async (req, res) => {
-  const username = await req.body.username;
-  const user = { user: username };
-  jwt.sign(user, process.env.ACCESS_SECRET_TOKEN);
-  res.json({ accessToken: accessToken });
+  try {
+    const { username } = req.body;
+    const accessToken = jwt.sign(
+      { user: username },
+      process.env.ACCESS_SECRET_TOKEN
+    );
+    res.json({ accessToken: accessToken });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+    console.error(error);
+  }
 });
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader;
+  if (token == null) return res.status(401);
+
+  jwt.verify(token, process.env.ACCESS_SECRET_TOKEN, (err, user) => {
+    if (err) return res.status(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is listening on port ${PORT}`);
-=======
-app.listen(PORT, () => {
-  console.log(`app is listening on port ${PORT}`);
->>>>>>> setup-branch
 });
